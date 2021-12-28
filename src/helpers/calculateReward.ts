8dc7346be751adb8ef4d49e830f393b3467fefae
dmcong <83624415+dmcong@users.noreply.github.com>
2021-12-28 16:27:27 +0700

    Update calculateReward.ts

--- src/helpers/calculateReward.ts
+++ src/helpers/calculateReward.ts
@@ -1,21 +1,21 @@
-import { utils, DebtData, FarmData } from '@senswap/sen-js'
+import { utils } from '@senswap/sen-js'
 
-const calculateReward = (debt: DebtData, farm: FarmData): number => {
+const calculateReward = (debt, farm) => {
   if (debt === null || Object.keys(debt).length === 0) return 0
   if (farm === null || Object.keys(farm).length === 0) return 0
-  const p =
-    Number(utils.undecimalize(farm.reward, 9)) /
-    Number(utils.undecimalize(farm.total_shares, 9))
-  const t =
-    (Date.now() / 1000 - Number(farm.genesis_timestamp)) / Number(farm.period)
-  const D = Number(utils.undecimalize(farm.compensation, 18)) // fixed
+  if (!farm.total_shares) return 0
 
-  const r_i = Number(utils.undecimalize(debt.shares, 9))
-  const d_i = Number(utils.undecimalize(debt.debt, 9))
-
-  const reward = p * Math.floor(t) * r_i - d_i + D * r_i
-
-  return Number(Number(reward).toFixed(8))
+  const shares = debt.shares
+  const compensation = farm.compensation
+  const delay =
+    (global.BigInt(Math.floor(Date.now() / 1000)) - farm.genesis_timestamp) /
+    farm.period
+  const precision = global.BigInt(10 ** 18)
+  const rewardsPerShare = (precision * farm.reward) / farm.total_shares
+  const newDebt =
+    ((rewardsPerShare * delay + compensation) * shares) / precision
+  const yieldFarm = utils.undecimalize(newDebt - debt.debt, 9)
+  return Number(yieldFarm)
 }
 
 export default calculateReward
