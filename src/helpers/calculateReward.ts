3f57f1ad3e096a9dffd013ae50fca13cbac81e31
dmcong <83624415+dmcong@users.noreply.github.com>
2021-12-28 16:31:54 +0700

    Update calculateReward.ts

--- src/helpers/calculateReward.ts
+++ src/helpers/calculateReward.ts
@@ -1,6 +1,6 @@
-import { utils } from '@senswap/sen-js'
+import { DebtData, FarmData, utils } from '@senswap/sen-js'
 
-const calculateReward = (debt, farm) => {
+const calculateReward = (debt: DebtData, farm: FarmData) => {
   if (debt === null || Object.keys(debt).length === 0) return 0
   if (farm === null || Object.keys(farm).length === 0) return 0
   if (!farm.total_shares) return 0
