7322e7fe2b7d5a084ff0f19aad25f423498de7d9
tuanle161099 <tuanle@descartes.network>
2021-12-28 09:54:32 +0700

    -

--- src/app/widget/index.tsx
+++ src/app/widget/index.tsx
@@ -5,18 +5,18 @@ import SearchBar from './searchBar'
 import Stacked from './stacked'
 
 const Widget = () => {
-  const [hidden, setHidden] = useState(true)
+  const [toggle, setToggle] = useState(true)
 
   return (
     <Fragment>
       <Button
         className="button-search"
         type="text"
-        icon={<IonIcon name={hidden ? 'search-outline' : 'close-outline'} />}
-        onClick={() => setHidden(!hidden)}
+        icon={<IonIcon name={toggle ? 'search-outline' : 'close-outline'} />}
+        onClick={() => setToggle(!toggle)}
       />
-      <SearchBar isHidden={hidden} />
-      <Tabs className={hidden ? '' : 'hidden-tab'} defaultActiveKey="1">
+      <SearchBar isHidden={toggle} />
+      <Tabs className={toggle ? '' : 'hidden-tab'} defaultActiveKey="1">
         <Tabs.TabPane tab="Stacked farms" key="stacked-farm">
           <Stacked />
         </Tabs.TabPane>
