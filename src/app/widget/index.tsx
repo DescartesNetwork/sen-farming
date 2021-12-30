55ec2bf615ca9d51d201002fd87578b40579541c
dmcong <83624415+dmcong@users.noreply.github.com>
2021-12-30 11:44:37 +0700

    fix lazy load

--- src/app/widget/index.tsx
+++ src/app/widget/index.tsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { forceCheck } from '@senswap/react-lazyload'
 
 import { Button, Tabs } from 'antd'
 import IonIcon from 'shared/antd/ionicon'
@@ -33,7 +34,14 @@ const Widget = () => {
       />
       <SearchBar isHidden={!search && !isOpenSearch} />
       <FarmWatcher style={{ height: 336 }}>
-        <Tabs className={!isOpenSearch ? '' : 'hidden-tab'}>
+        <Tabs
+          className={!isOpenSearch ? '' : 'hidden-tab'}
+          onChange={() =>
+            setTimeout(() => {
+              forceCheck()
+            }, 500)
+          }
+        >
           <Tabs.TabPane tab="Staked farms" key="staked-farm">
             <StakedFarms />
           </Tabs.TabPane>
