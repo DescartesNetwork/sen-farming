3513fb74323bd7141a941f38c8d9d9f46bf0b655
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-01-05 17:51:14 +0700

    Auto change active tab when directing from  Sen LP

---
+++ src/app/hooks/useCheckActiveTab.ts
@@ -0,0 +1,40 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useSentreFarms } from './listFarm/useSentreFarms'
+import { useStakedFarms } from './listFarm/useStakedFarms'
+import { useYourFarms } from './listFarm/useYourFarms'
+import { useComunityFarms } from './listFarm/useCommunityFarms'
+import { useSearchFarm } from './useSearchFarm'
+
+export const useCheckActiveTab = () => {
+  const [activeTab, setActiveTab] = useState('')
+  const { sentreFarms } = useSentreFarms()
+  const { stakedFarms } = useStakedFarms()
+  const { yourFarms } = useYourFarms()
+  const { communityFarms } = useComunityFarms()
+
+  const searchedComFarms = useSearchFarm(communityFarms)
+  const searchedSenFarms = useSearchFarm(sentreFarms)
+  const searchedStakedFarms = useSearchFarm(stakedFarms)
+  const searchedYourFarms = useSearchFarm(yourFarms)
+
+  const checkTab = useCallback(async () => {
+    if (!!Object.keys(searchedSenFarms).length) return setActiveTab('sen-farms')
+    if (!!Object.keys(searchedYourFarms).length)
+      return setActiveTab('your-farms')
+    if (!!Object.keys(searchedStakedFarms).length)
+      return setActiveTab('staked-farms')
+    if (!!Object.keys(searchedComFarms).length)
+      return setActiveTab('community-farms')
+  }, [
+    searchedComFarms,
+    searchedSenFarms,
+    searchedStakedFarms,
+    searchedYourFarms,
+  ])
+
+  useEffect(() => {
+    checkTab()
+  }, [checkTab])
+
+  return activeTab
+}
