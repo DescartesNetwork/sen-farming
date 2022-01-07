3f24130b8b7ab90f102bd8c5a9cd8c24848eb75c
dmcong <83624415+dmcong@users.noreply.github.com>
2022-01-07 11:39:41 +0700

    fix performance budget

--- src/app/hooks/useBudget.ts
+++ src/app/hooks/useBudget.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { utils } from '@senswap/sen-js'
 
@@ -15,22 +15,25 @@ export const useBudget = (
   } = useSelector((state: AppState) => state)
   const { treasury_stake, treasury_reward, mint_reward, total_shares } =
     farmData || {}
-  const decimal = useMintDecimals(mint_reward) || 0
+  const decimal = useMintDecimals(mint_reward)
   const { symbol: budgetSymbol } = useMintCgk(mint_reward)
 
-  useEffect(() => {
-    ;(async () => {
-      try {
-        const { splt } = window.sentre
-        let { amount } = await splt.getAccountData(treasury_reward)
-        if (treasury_reward === treasury_stake) amount = amount - total_shares
-        const budget = Number(utils.undecimalize(amount, decimal))
-        setBudget(budget)
-      } catch (er) {
-        setBudget(0)
-      }
-    })()
+  const fetchBudgetData = useCallback(async () => {
+    if (decimal === undefined) return setBudget(0)
+    try {
+      const { splt } = window.sentre
+      let { amount } = await splt.getAccountData(treasury_reward)
+      if (treasury_reward === treasury_stake) amount = amount - total_shares
+      const budget = Number(utils.undecimalize(amount, decimal))
+      setBudget(budget)
+    } catch (error) {
+      setBudget(0)
+    }
   }, [decimal, total_shares, treasury_reward, treasury_stake])
+
+  useEffect(() => {
+    fetchBudgetData()
+  }, [fetchBudgetData])
 
   return { budget, budgetSymbol }
 }
