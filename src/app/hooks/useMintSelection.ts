82622ec973f7b570dcedf070564f3f994d98c4a8
ngduytradescartes <95331134+ngduytradescartes@users.noreply.github.com>
2022-01-12 16:22:00 +0700

    Search token in New Farming

--- src/app/hooks/useMintSelection.ts
+++ src/app/hooks/useMintSelection.ts
@@ -1,24 +1,30 @@
 import { useCallback, useMemo, useState } from 'react'
 
-import { useAccount } from 'senhub/providers'
+import { useAccount, useMint } from 'senhub/providers'
+
+const KEYSIZE = 3
 
 export const useMintSelection = () => {
   const [searchedResult, setSearchedResult] = useState<string[]>()
   const { accounts } = useAccount()
+  const { tokenProvider } = useMint()
 
   const accountAddresses = useMemo(() => Object.keys(accounts), [accounts])
   const onSearch = useCallback(
-    (mintAddresses: string[] | undefined) => {
-      if (!mintAddresses) return setSearchedResult(undefined)
-      let searchedAccountAddresses = accountAddresses.filter(
-        (accountAddress) => {
-          const { mint } = accounts[accountAddress]
-          return mintAddresses.includes(mint)
-        },
-      )
-      return setSearchedResult(searchedAccountAddresses)
+    async (keyword: string | undefined) => {
+      if (!keyword || keyword.length < KEYSIZE)
+        return setSearchedResult(undefined)
+      const raw = await tokenProvider.find(keyword)
+      const searchedTokenAddress = raw.map(({ address }) => address)
+      let searchedResult: string[] = []
+      // Filter Tokens
+      accountAddresses.forEach((accAddr) => {
+        if (!searchedTokenAddress.includes(accounts[accAddr]?.mint)) return
+        return searchedResult.push(accAddr)
+      })
+      return setSearchedResult(searchedResult)
     },
-    [accountAddresses, accounts],
+    [accountAddresses, accounts, tokenProvider],
   )
 
   return {
